4b012cf075a97dbe6de805b033b749577f86d37c
Muhannad <Muhannadalmughrabi233@gmail.com>
2021-05-05 03:36:07 +0300

    Done Code-challnenge03

--- python/code_challenges/array_binary_search/array_binary_search.py
+++ python/code_challenges/array_binary_search/array_binary_search.py
@@ -1,50 +1,30 @@
-# import math
-# def BinarySearch1(arr,value):
-#     test=arr[0]
-#     mid= math.floor((len(arr)/2))
-#     x=mid
-#     if type(value) != int:
-#         print ('Invalid input to the Array')
-#         return 'Invalid input to the Array'
-#     if type(arr) != list:
-#         print('Invalid type , please insert an array')
-#         return 'Invalid type , please insert an array'
-#     while test != value:
-#         test=arr[mid]
-#         if test == value:
-#             print ("line 15", x)
-#             return x
-#         elif test > value:
-#             newArrAfter=arr[:mid]
-#             mid= int(math.floor((len(newArrAfter)/2)))
-#             if mid != int(len(newArrAfter/2)):
-#                 x-=mid+1
-#                 print  (" line 21" ,x )
-#             else:
-#                 x-=mid
-#                 print (" line 23 ", x)
-#         elif test < value:
-#             newArrayBefore=arr[mid:]
-#             mid= math.floor((len(newArrayBefore)/2))
-#             if mid %2==0:
-#                 x+=mid
-#                 print ("line28", x )
-#             else:
-#                 x+=mid
-#                 print ("line 30", x )
-#         if mid <=0:
-#             return 'Unsuccessful , invalid input'
-
+import math
 def BinarySearch(arr,value):
-    if( value not in arr):
-        print(-1)
-        return -1
+    if not(isinstance(value, int) or isinstance(value, float)):
+        return ("it seems you are trying wrong input types , please insert any number")
+    if type(arr) != list:
+            return 'Invalid type , please insert an array'
     for x in arr:
-        if(x == value):
-            print (arr.index(x))
-            return arr.index(x)
+        if not(isinstance(x, int) or isinstance(x, float)):
+            return "Are you trying to fool me? please insert an array of numbers!"
+    test=arr[0]
+    mid=  math.floor((len(arr)/2))
+    x=mid
+    while test != value:
+        test=arr[mid]
+        if test == value:
+            return x
+        elif test > value:
+            arr=arr[:mid]
+            mid=  math.floor((len(arr)/2))
+            if mid != len(arr)/2:
+                x=x - (mid+1)
+            else:
+                x=x - mid
+        elif test < value:
+            arr=arr[mid:]
+            mid=  math.floor((len(arr)/2))
+            x=x+mid
 
-# BinarySearch1([4,8,15,16,23,42], 15)
-# BinarySearch([11,22,33,44,55,66,77], 90)
-# BinarySearch([1, 2, 3, 5, 6, 7], 4)
-# BinarySearch([4,8,15,16,23,42, 15 , 30 ,4554 , 2403], 15)
+        if mid == 0:
+            return -1
--- python/tests/test_array_binary_search.py
+++ python/tests/test_array_binary_search.py
@@ -9,3 +9,22 @@ def test_BinarySearch_2():
     actual= BinarySearch([11,22,33,44,55,66,77], 90)
     expected= -1
     assert actual==expected
+
+
+def test_BinarySearch_3():
+    actual= BinarySearch([1, 2, 3.5, 5, 6, 7], 3.5)
+    expected= 2
+    assert actual==expected
+
+
+
+def test_BinarySearch_4():
+    actual= BinarySearch(['str1' , 'str2'], 4)
+    expected= "Are you trying to fool me? please insert an array of numbers!"
+    assert actual==expected
+
+
+def test_BinarySearch_5():
+    actual= BinarySearch([4,8,15,23,42,55,64,69], '1')
+    expected= "it seems you are trying wrong input types , please insert any number"
+    assert actual==expected
